bc6d13c731a7e9ac80a9e3c0cfb8f6c9633cda2f
abarikdar <73364852+abarikdar@users.noreply.github.com>
2023-11-08 23:10:39 -0500

    added nature image api

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Assignment5 Api App
+
+For each API used on the site include :
+
+- API Provider
+- API Name (if any)
+- Screenshot of a successful API test in Postman for that endpoint
+- Link to the API documentation
+- Short description of what you used the API for on your site, anything that came up, or any issues you had
--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
-export default function App() {
-  return (
-    <div>Hello World!</div>
-  );
-}
+import React, { useState } from "react";
+
+const App = () => {
+  const [imageData, setImageData] = useState(null);
+  // const apiKey = ' '; 
+  const category = 'nature';
+
+  const handleGetPictureClick = async () => {
+    try {
+      const url = 'https://api.api-ninjas.com/v1/randomimage?category=' + category;
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'X-Api-Key': 'api key',
+          'Accept': 'image/jpg',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const result = await response.blob();
+              
+      setImageData(URL.createObjectURL(result));
+      } catch (error) {
+      console.error('Error:', error);
+      }
+  };
+
+    return (
+      <div>
+        <h2>Nature Background</h2>
+        <button className={'btn'} onClick={handleGetPictureClick}>Get Picture</button>
+        {imageData && (
+        <img src={imageData} alt="Nature Pic" />
+        )}
+        {/* <img src={imageData} alt="nature-pic" /> */}
+        {/* <h2>{pictureData.title}</h2>
+        <p>{pictureData.explanation}</p> */}
+      </div>
+    );
+};
+
+export default App;
